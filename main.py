ce6a79c4a88ee22fce45f5671b39a917e7407721
sebastianhcarboni25 <sebastianocarboni25@gmail.com>
2025-07-09 15:44:57 +0900

    main.py

--- main.py
+++ main.py
@@ -114,3 +114,8 @@ def handle_message(event):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
